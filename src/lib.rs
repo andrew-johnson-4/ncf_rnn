05fb470ac9c1d691bcf3ee7d2623c2145f2f9790
Andrew Johnson <andrew@subarctic.org>
2021-02-25 20:35:09 -0700

    do some implementation of small semantics

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,28 @@
 use autograd as ag;
 
+
 pub struct ParseResult {
 }
+
 impl ParseResult {
    pub fn probability(&self) -> f64 {
       0.0
    }
 }
+
+
+pub struct ParseLine {
+}
+impl ParseLine {
+   pub fn poke(&self, c: char) -> Vec<ParseLine> {
+      Vec::new()
+   }
+
+   pub fn probability(&self) -> f64 {
+      1.0
+   }
+}
+
 
 pub struct ProbabilisticGrammar {
    //The dropdown_penalty hyper-parameter discourages parse lines that introduce many grammar nodes
@@ -14,8 +30,9 @@ pub struct ProbabilisticGrammar {
 
    //The max_lines hyper-parameter is the maximum number of active parse lines for a parse attempt
    //Extra lines over this limit will be pruned based on their perplexity score
-   max_lines: u64,
+   max_lines: usize,
 }
+
 impl Default for ProbabilisticGrammar {
     fn default() -> Self {
        ProbabilisticGrammar {
@@ -24,12 +41,26 @@ impl Default for ProbabilisticGrammar {
        }
     }
 }
+
 impl ProbabilisticGrammar {
    pub fn load<P: AsRef<std::path::Path>>(encf: P) -> Self {
       Default::default()
    }
+
    pub fn train<P: AsRef<std::path::Path>>(&self, dat: P) {} 
+
    pub fn recognize(&self, cs: &str) -> ParseResult {
+      let mut lines: Vec<ParseLine> = vec![ /*grammar ROOT*/ ];
+      for c in cs.chars() {
+         let mut new_lines: Vec<ParseLine> = Vec::new();
+         for l in lines.iter() {
+            new_lines.append(&mut l.poke(c));
+         }
+         new_lines.sort_by(|a,b| a.probability().partial_cmp(&b.probability()).unwrap() );
+         new_lines.reverse();
+         new_lines.truncate(self.max_lines);
+         lines = new_lines;
+      }
       ParseResult {}
    }
 }
